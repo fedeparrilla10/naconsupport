cb4a62fb28598b36899a9a026da408c182a05f7a
fedeparrilla10 <federicoivanpd@gmail.com>
2024-11-18 13:54:39 +0100

    [fix] className in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer class="pt-10 pb-2 px-10 flex justify-center absolute bottom-0 items-center w-full z-40 text-xs">
+    <footer className="pt-10 pb-2 px-10 flex justify-center absolute bottom-0 items-center w-full z-40 text-xs">
       ® Sitelicon Ecommerce Services - 2024
     </footer>
   );

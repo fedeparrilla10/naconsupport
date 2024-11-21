be8b3e91b6114ec82dadcc7734adfdd4ce2d5590
fedeparrilla10 <federicoivanpd@gmail.com>
2024-11-21 13:24:43 +0100

    [update] accordion before pdf in faq

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -17,26 +17,6 @@ const FAQ = ({ message, question, handleOptionSelect }) => {
       </div>
 
       <div className="flex flex-col items-center gap-8 w-full">
-        <div className="flex flex-col flex-wrap justify-center items-center md:flex-row gap-4">
-          {PDFs.map((faq) => {
-            return (
-              <a href={faq.url} key={faq.name} target="_blank">
-                <button
-                  className="flex flex-row items-center border-2 border-gray-50 w-[350px] md:w-[180px] gap-4 cursor-pointer rounded-lg hover:bg-gray-700 hover:text-white 
-          transition duration-300 py-4 px-2"
-                >
-                  <img
-                    src="/naconsupport/pdf.svg"
-                    alt="Realizar una consulta"
-                    width={30}
-                    height={30}
-                  />
-                  <p className="text-center">{faq.name}</p>
-                </button>
-              </a>
-            );
-          })}
-        </div>
         <div className="w-full md:w-1/2">
           {frecuentlyAsked.map((faq) => {
             return (
@@ -78,6 +58,27 @@ const FAQ = ({ message, question, handleOptionSelect }) => {
             );
           })}
         </div>
+        <div className="flex flex-col flex-wrap justify-center items-center md:flex-row gap-4">
+          {PDFs.map((faq) => {
+            return (
+              <a href={faq.url} key={faq.name} target="_blank">
+                <button
+                  className="flex flex-row items-center border-2 border-gray-50 w-[350px] md:w-[180px] gap-4 cursor-pointer rounded-lg hover:bg-gray-700 hover:text-white 
+          transition duration-300 py-4 px-2"
+                >
+                  <img
+                    src="/naconsupport/pdf.svg"
+                    alt="Realizar una consulta"
+                    width={30}
+                    height={30}
+                  />
+                  <p className="text-center">{faq.name}</p>
+                </button>
+              </a>
+            );
+          })}
+        </div>
+
         <div className="flex flex-col xl:flex-row gap-12 md:gap-8 pt-2">
           {videos.map((video) => {
             return (
